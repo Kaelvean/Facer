e77be028369e20ebc7959a54afdbfb4d98fc5176
尤恩 <85486083+Kaelvean@users.noreply.github.com>
2023-03-14 13:21:27 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,9 +4,7 @@ module.exports = {
     lastUpdated: true,
     cleanUrls: true,
     head: [
-        [
-            'link', { rel: 'icon', href: '/images/logo.png' }
-        ],
+
         [
             'link', { rel:'shortcut icon', href:'/images/favicon.ico'}
         ]
